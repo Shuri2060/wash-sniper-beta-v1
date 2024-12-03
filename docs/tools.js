c8edbe8276d514627c3792223deee92ea5282a9d
Shuri2060 <29679284+Shuri2060@users.noreply.github.com>
2024-12-03 13:38:05 +0000

    tools display better null refer

--- docs/tools.js
+++ docs/tools.js
@@ -325,10 +325,10 @@
                             const resp = await Hyperliquid.requests.postInfoAsync({ url: URLS[IS_MAINNET], payload })
                             const referred = resp.referrerState.data?.referralStates?.reduce((s, user) => s + `${user.user} | ${new Date(user.timeJoined).toISOString()} | ${String(user.cumVlm).padStart(15)} | ${String(user.cumRewardedFeesSinceReferred).padStart(15)} | ${String(user.cumFeesRewardedToReferrer).padStart(15)}\n`, '') || '\n'
                             elements.tools.other.referDisplay.element.value = `Address:\t\t${address}
-Referred By:\t\t${resp.referredBy?.referrer} (code ${resp.referredBy?.code})
+Referred By:\t\t${resp.referredBy?.referrer || 'none'} (code ${resp.referredBy?.code || 'N/A'})
 Cumulative Volume:\t${resp.cumVlm}
 
-Referral Code:\t\t${resp.referrerState.data?.code}
+Referral Code:\t\t${resp.referrerState.data?.code || ''}
 ${'Referred'.padEnd(42)} | ${'Joined'.padEnd(24)} | ${'Volume'.padEnd(15)} | ${'Fees Paid'.padEnd(15)} | ${'Rewards'.padEnd(15)}
 ${'='.repeat(42 + 24 + 15 * 3 + 3 * 4)}
 ${referred}
